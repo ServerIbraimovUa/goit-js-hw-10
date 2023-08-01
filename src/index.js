3b6239286a28895f431e1f975a89cf85c0bf6263
ServerIbraimovUa <serveribraimov7@gmail.com>
2023-08-01 18:13:16 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import SlimSelect from 'slim-select';
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import { createMarkup, createMarkupCat } from './js/template/createMarkup';
 import refs from './js/refs';
@@ -7,8 +9,10 @@ refs.selectEl.addEventListener('change', onValueId);
 fetchBreeds()
   .then(arr => {
     load();
+
     return (refs.selectEl.innerHTML = createMarkup(arr.data));
   })
+  .then(() => slim())
   .catch(fetchError);
 
 function onValueId(e) {
@@ -16,17 +20,26 @@ function onValueId(e) {
   fetchCatByBreed(id)
     .then(obj => {
       load();
+
       return (refs.catInfoEl.innerHTML = createMarkupCat(obj.data));
     })
+    .then(() => success())
     .catch(fetchError);
 }
 function fetchError() {
   //   refs.error.hidden = false;
   Report.failure(refs.error.textContent, '');
 }
-
+function success() {
+  Notify.success('Search was successful!)', '');
+}
 function load() {
   refs.selectEl.hidden = false;
-
   refs.loaderEl.classList.remove('loader');
+}
+
+function slim() {
+  new SlimSelect({
+    select: refs.selectEl,
+  });
 }
